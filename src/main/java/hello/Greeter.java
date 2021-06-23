bb9e43091e2169f95d62d3b7b4b7ece110e76072
RubinMathew <78633996+RubinMathew@users.noreply.github.com>
2021-06-23 12:29:29 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world----- Rubin Mathew !! Hi or Bye is the string needed here....";
+    return "Hello world----- Rubin Mathew !! Hi or  is the string needed here....";
   }
 }
